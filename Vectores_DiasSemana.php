c04d9cedfcf7d526e36bd0bacbeed1235a9c04a2
Francisco <72940617+frangf@users.noreply.github.com>
2020-10-25 17:56:12 +0100

    Add files via upload

---
+++ Vectores_DiasSemana.php
@@ -0,0 +1,11 @@
+<?php
+$weekday  =  array("Mondey","Thuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
+echo "Hoy es " .$weekday[0];
+echo "\nHoy es " .$weekday[1];
+echo "\nHoy es " .$weekday[2];
+echo "\nHoy es " .$weekday[3];
+echo "\nHoy es " .$weekday[4];
+echo "\nHoy es " .$weekday[5];
+echo "\nHoy es " .$weekday[6];
+?> 
