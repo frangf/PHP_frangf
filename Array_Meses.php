a82bcacf523b08f857987e506afa72b740db718f
frangf <franexyt@gmail.com>
2020-11-05 19:58:02 +0100

    Prueba

---
+++ Array_Meses.php
@@ -0,0 +1,9 @@
+<?php
+#Creacion del vector
+$Meses=array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+foreach ($Meses as $mes)
+{
+    echo "$mes ";
+}
+?>
